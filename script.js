1d9ab99cc0bdb5080aa0d041ce33b641e7dce9d9
serzmerz <serzmerz@gmail.com>
2018-05-13 00:46:30 +0300

    add function save to image/ import/export

--- script.js
+++ script.js
@@ -1,73 +1,139 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 ctx.clearRect(0, 0, canvas.width, canvas.height);
-const RegularForm = document.querySelector('#regularForm');
-RegularForm.addEventListener("submit", submitRegularHandler);
+drawLayout(ctx);
 
+let regularFormData = {}
+const RegularForm = document.querySelector('#regularForm');
+
+let optimizedFormData = {}
 const OptimizedForm = document.querySelector('#optimizedForm');
-OptimizedForm.addEventListener("submit", submitOptimizedHandler);
+
+const submitBtn = document.querySelector('.submitButton');
+submitBtn.addEventListener("click", submitHandler)
+
+const downloadLink = document.querySelector('.image');
+downloadLink.addEventListener("click", downloadImage);
+
+const downloadFileLink = document.querySelector('.file');
+downloadFileLink.addEventListener("click", downloadFile);
+
+const importLink = document.querySelector('#selectFiles');
+importLink.addEventListener("change", uploadFile, false);
+
+function uploadFile () {
+  const fr = new FileReader();
+
+  fr.onload = function(e) {
+    const result = JSON.parse(e.target.result);
+    console.log(result)
+    regularFormData = result.regularFormData;
+    optimizedFormData = result.optimizedFormData;
+    startDrawing();
+  }
+
+  fr.readAsText(this.files[0]);
+}
+
+function downloadImage () {
+  downloadLink.href = canvas.toDataURL();
+  downloadLink.download = "schedule.png";
+}
+
+function downloadFile () {
+  const file = new Blob([JSON.stringify({ regularFormData, optimizedFormData })], { type: 'application/json' });
+  downloadFileLink.href = URL.createObjectURL(file);
+  downloadFileLink.download = "data.json";
+}
 
 const colors = ["rgb(25, 151, 240)", "rgb(252, 79, 140)", "rgb(254, 228, 72)", "rgb(31, 202, 37)"];
 
-function submitRegularHandler (e) {
-  e.preventDefault()
-  drawRegularSchedule(RegularForm, 200)
+function serializeFormData (form) {
+  return {
+    sleepFrom: form.sleepFrom.value,
+    sleepTo: form.sleepTo.value,
+    wayToWorkFrom: form.wayToWorkFrom.value,
+    wayToWorkTo: form.wayToWorkTo.value,
+    wayToWorkTransport: form.wayToWorkTransport.value,
+    workStart: form.workStart.value,
+    workEnd: form.workEnd.value,
+    wayFromWorkFrom: form.wayFromWorkFrom.value,
+    wayFromWorkTo: form.wayFromWorkTo.value,
+    wayFromWorkTransport: form.wayFromWorkTransport.value,
+    activityAtHomeFrom: form.activityAtHomeFrom.value,
+    activityAtHomeTo: form.activityAtHomeTo.value,
+  }
 }
 
-function submitOptimizedHandler (e) {
-  e.preventDefault()
-  drawRegularSchedule(OptimizedForm, 600)
+function submitHandler () {
+  regularFormData = serializeFormData(RegularForm);
+  optimizedFormData = serializeFormData(OptimizedForm);
+  startDrawing();
 }
 
-function drawRegularSchedule (form, startY) {
+function startDrawing () {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawLayout(ctx);
-  const from = form.sleepFrom.value;
-  const to = form.sleepTo.value;
-  if (to > 5) {
-    drawTask(ctx, from, 5, 'SLEEP', "rgb(25, 151, 240)", startY);
-    drawTask(ctx, 5, to, 'SLEEP', "rgb(25, 151, 240)", startY);
-  } else {
-    drawTask(ctx, from, to, 'SLEEP', "rgb(25, 151, 240)", startY);
+  drawSchedule(regularFormData, 200);
+  drawSchedule(optimizedFormData, 600);
+}
+
+function drawSchedule (form, startY) {
+  if (form.sleepFrom && form.sleepTo) {
+    const from = form.sleepFrom;
+    const to = form.sleepTo;
+    if (to > 5) {
+      drawTask(ctx, from, 5, 'SLEEP', "rgb(25, 151, 240)", startY);
+      drawTask(ctx, 5, to, 'SLEEP', "rgb(25, 151, 240)", startY);
+    } else {
+      drawTask(ctx, from, to, 'SLEEP', "rgb(25, 151, 240)", startY);
+    }
   }
 
-  drawTask(ctx, form.wayToWorkFrom.value, form.wayToWorkTo.value, '', "rgb(252, 79, 140)", startY)
-  drawFlow(
-    ctx,
-    form.wayToWorkFrom.value,
-    form.wayToWorkTo.value,
-    form.wayToWorkTransport.value.split(' '),
-    "transparent",
-    200
-  )
+  if (form.wayToWorkFrom && form.wayToWorkTo) {
+    drawTask(ctx, form.wayToWorkFrom, form.wayToWorkTo, '', "rgb(252, 79, 140)", startY)
+    drawFlow(
+      ctx,
+      form.wayToWorkFrom,
+      form.wayToWorkTo,
+      form.wayToWorkTransport.split(' '),
+      "transparent",
+      startY
+    )
+  }
 
+  if (form.workStart && form.workEnd) {
+    drawFlowTask(ctx, form.workStart, form.workEnd, '', "rgb(31, 202, 37)", startY)
+    drawFlow(ctx, form.workStart, form.workEnd, ['ACTIVITY AT WORK'], "rgba(31, 202, 37, .2)", startY)
+  }
 
-  drawFlowTask(ctx, form.workStart.value, form.workEnd.value, '', "rgb(31, 202, 37)", startY)
-  drawFlow(ctx, form.workStart.value, form.workEnd.value, ['ACTIVITY AT WORK'], "rgba(31, 202, 37, .2)", startY)
-  drawTask(ctx, form.wayFromWorkFrom.value, form.wayFromWorkTo.value, '', "rgb(252, 79, 140)", startY)
-  drawFlow(
-    ctx,
-    form.wayFromWorkFrom.value,
-    form.wayFromWorkTo.value,
-    form.wayFromWorkTransport.value.split(' '),
-    "transparent",
-    startY
-  )
+  if (form.wayFromWorkFrom && form.wayFromWorkTo) {
+    drawTask(ctx, form.wayFromWorkFrom, form.wayFromWorkTo, '', "rgb(252, 79, 140)", startY)
+    drawFlow(
+      ctx,
+      form.wayFromWorkFrom,
+      form.wayFromWorkTo,
+      form.wayFromWorkTransport.split(' '),
+      "transparent",
+      startY
+    )
+  }
 
-  drawFlowTask(ctx, form.activityAtHomeFrom.value, form.activityAtHomeTo.value, '', "rgb(31, 202, 37)", startY)
-  drawFlow(ctx, form.activityAtHomeFrom.value, form.activityAtHomeTo.value, ['ACTIVITY AT HOME'], "rgba(31, 202, 37, .2)", startY)
+  if (form.activityAtHomeFrom && form.activityAtHomeTo) {
+    drawFlowTask(ctx, form.activityAtHomeFrom, form.activityAtHomeTo, '', "rgb(31, 202, 37)", startY)
+    drawFlow(ctx, form.activityAtHomeFrom, form.activityAtHomeTo, ['ACTIVITY AT HOME'], "rgba(31, 202, 37, .2)", startY)
+  }
+
   drawScale(ctx, startY);
 }
 
 function drawTask(ctx, start, end, text = '', color, startY = 30) {
   ctx.save();
   ctx.fillStyle = color;
-  console.log('s', convertHour(start))
-  console.log('e', convertHour(end))
   let endTime = convertHour(end)
   if (convertHour(end) === 0) {
     endTime = (convertHour(end) < convertHour(start)) ? 24 : 0
   }
-  console.log('eee', endTime)
     for(let i = convertHour(start); i < endTime; i++) {
     ctx.fillRect(i*100 + 30,startY + 30,25,startY - 170);
     ctx.fillRect(i*100 + 30+25,startY + 30,25,startY - 170);
@@ -87,10 +153,10 @@ function drawFlowTask(ctx, start, end, text, color, startY = 30) {
   ctx.save();
   ctx.fillStyle = color;
   for(let i = convertHour(start); i < convertHour(end); i++) {
-    ctx.fillRect(i*100 + 30,startY + 10,25,startY - 150);
-    ctx.fillRect(i*100 + 30+25,startY + 10,25,startY - 150);
-    ctx.fillRect(i*100 + 30+50,startY + 10,25,startY - 150);
-    ctx.fillRect(i*100 + 30+75,startY + 10,25,startY - 150);
+    ctx.fillRect(i*100 + 30,startY + 10,25, 50);
+    ctx.fillRect(i*100 + 30+25,startY + 10,25, 50);
+    ctx.fillRect(i*100 + 30+50,startY + 10,25, 50);
+    ctx.fillRect(i*100 + 30+75,startY + 10,25, 50);
   }
   ctx.textAlign="left";
   ctx.font = 'bold 10px Arial';
@@ -104,7 +170,7 @@ function drawFlowTask(ctx, start, end, text, color, startY = 30) {
 function drawFlow(ctx, start, end, textArray, textColor, startY = 30) {
   ctx.fillStyle = textColor;
   const width = (convertHour(end) *100 + 30) - (convertHour(start) * 100 + 30);
-  ctx.fillRect(convertHour(start)*100 + 30, startY + 60, width, startY - 150);
+  ctx.fillRect(convertHour(start)*100 + 30, startY + 60, width, 50);
   ctx.fillStyle = "#000";
   ctx.font = 'bold 10px Arial';
   ctx.lineWidth = 3;
@@ -152,6 +218,7 @@ function drawLayout(ctx) {
 }
 
 function drawScale(ctx, startY = 30) {
+  ctx.save();
   ctx.fillStyle = "#000";
   ctx.font = '15px Arial';
   ctx.textAlign = 'center';
@@ -159,7 +226,7 @@ function drawScale(ctx, startY = 30) {
   for(let i = 0; i < 25; i++) {
     ctx.moveTo(i * 100 + 30, startY);
     ctx.lineTo(i * 100 + 30, startY + 60);
-    ctx.fillText(makeHours(i + 5),i*100 + 30, startY - 10);
+    ctx.fillText(makeHours(i + 5), i*100 + 30, startY - 10);
   }
   ctx.stroke();
   ctx.lineWidth = 1;
@@ -172,6 +239,7 @@ function drawScale(ctx, startY = 30) {
     ctx.lineTo(i*100 + 30 + 75, startY + 60);
   }
   ctx.stroke();
+  ctx.restore();
 }
 
 function convertHour(hour) {
